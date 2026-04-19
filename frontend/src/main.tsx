680832b32ca9bc6cd2f20579236ad404e92b49e2
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-04-19 18:33:09 +0300

    Setting up TanStack Query provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { theme } from './theme'
-import { queryClient } from './app/queryClient'
+import { QueryProvider } from './app/QueryProvider'
 import { Router } from './app/Router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
+      <QueryProvider>
         <Router />
-      </QueryClientProvider>
+      </QueryProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
