3c73eed9756ec797f30d7ee059fbf540bf816380
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-04-23 09:58:56 +0300

    Adding price option to sortby query

--- shared/src/schemas/getDishesQuery.ts
+++ shared/src/schemas/getDishesQuery.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const GetDishesQuerySchema = z.object({
   filter: z.enum(["active", "inactive", "all"]).optional(),
   search: z.string().optional(),
-  sortBy: z.enum(["name", "quantity"]).optional(),
+  sortBy: z.enum(["name", "quantity", "price"]).optional(),
   sortOrder: z.enum(["asc", "desc"]).optional(),
 });
 
